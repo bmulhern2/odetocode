c669a69c6bffd168ca25217b206383791d045dd2
System Administrator <bmulhern09@gmail.com>
2020-07-04 14:30:09 -0400

    deploy

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// install dotenv && configure
+// 
+
 let express = require('express')
 let http = require('http')
 let mongoose = require('mongoose')
@@ -5,7 +8,9 @@ let bodyParser = require('body-parser')
 let uri = "mongodb+srv://bmulhern2:Bmole22%21%21@cluster0-eopst.mongodb.net/<dbname>?retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 let Schema = mongoose.Schema;
+let ObjectId = Schema.ObjectId;
 let PostSchema = new Schema({
+    _id: ObjectId,
     description: String,
     post: String
     // Date
@@ -17,8 +22,8 @@ let app = express()
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', function (req, res) {
-    Posts.find({}, function(err, posts) {
+app.get('/', async function (req, res) {
+    await Posts.find({}, function(err, posts) {
         if(!err) {
     res.render("index", { posts: posts })
         } else {
@@ -29,11 +34,23 @@ app.get('/', function (req, res) {
 app.get('/admin', function (req, res) {
     res.render('admin')
 })
-app.post('/post', function (req, res) {
-    let newPost = new Posts(req.body)
-    Posts.create(newPost, function() {
-        console.log(newPost)
+app.post('/post/create', function (req, res) {
+    let newPost = new Posts({
+        description: req.body.description,
+        post: req.body.post,
+        _id: _id
     })
+    Posts.create(newPost)
     res.redirect('/')
+})
+app.get('/post/:id', async function (req, res) {
+    await Posts.findById({_id: req.params.id}, function(posts) {
+        try {
+            console.log(posts)
+            res.render('post', { posts: posts })
+        } catch (error) {
+            console.log(error)
+        }
+    })
 })
 http.createServer(app).listen('8080')

7b4e36f584ec97faac2276856f7e16faff8b97aa
System Administrator <bmulhern09@gmail.com>
2020-07-05 16:28:32 -0400

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-// LightHouse Audit
-// Google Listing
-// Custom Domain
+/*
+   Apache License
+   Version 2.0, January 2004
+   http://www.apache.org/licenses/
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES  CONDITIONS OF  KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// LightHouse
+// Author 
+// Video
+// More Articles
 let dotenv = require('dotenv')
 dotenv.config()
 let express = require('express')

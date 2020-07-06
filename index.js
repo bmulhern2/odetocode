ecab45733d4afb70e04ec07b8505a7273c92db60
System Administrator <bmulhern09@gmail.com>
2020-07-06 11:20:49 -0400

    Deploy

--- index.js
+++ index.js
@@ -12,7 +12,14 @@
 
 //To Do
 
-// All 100s Lighthouse Article
+/*
+All 100s lighthouse article
+(forEach the nav bar)
+Single picture schema: logo
+Video of commands + link field
+Style tag from index to post
+Email list for new posts with SendGrid
+*/
 
 let dotenv = require('dotenv')
 dotenv.config()
@@ -27,6 +34,11 @@ let upload = multer({ storage: storage })
 let cookieParser = require('cookie-parser')
 let Schema = mongoose.Schema;
 let ObjectId = Schema.ObjectId;
+let LogoSchema = new Schema({
+    logo: String,
+    id: ObjectId
+})
+let Logo = mongoose.model('Logo', LogoSchema)
 let PostSchema = new Schema({
     id: ObjectId,
     description: String,
@@ -42,14 +54,17 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.get('/', async function (req, res) {
-    await Posts.find({}, function(err, posts) {
+app.get('/', function (req, res) {
+    Posts.find({}, function(err, posts) {
         if(!err) {
-    res.render("index", { posts: posts })
+    res.render("index", { posts: posts})
         } else {
             console.log(err)
         }
     })
+})
+app.get('/admin/logo', function (req, res) {
+    res.render('logo')
 })
 app.get('/admin', function (req, res) {
     res.render('admin')
@@ -67,6 +82,15 @@ app.post('/post/create', upload.single('image'), function (req, res) {
     })
     Posts.create(newPost)
     res.redirect('/')
+})
+app.post("/logo/create", upload.single('image'), function(req, res) {
+    let logo = req.file.buffer.toString('base64')
+    let newlogo = new Logo({
+        logo: logo,
+        id: req.body.id
+    })
+    Logo.create(newlogo)
+    res.redirect("/")
 })
 app.get('/post/:id', function (req, res) {
     Posts.findById(req.params.id, function(err, Post) {
